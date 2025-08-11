16ce6a44ecf6fe8e90c70fdf204ce25d647c70ff
Łukasz Korbel <lkorbel@tuta.io>
2025-08-11 19:03:25 +0700

    update comments in git-update

--- git/git-update.sh
+++ git/git-update.sh
@@ -4,19 +4,20 @@
 # GIT-UPDATE
 #
 # Łukasz Korbel 2016
-# korbel85@gmail.com
+# lkorbel@tuta.io
 #
 # Convenience script - automatically pull all changes, rebase local changes on them then and push to remote repository.
-
-# Support usage of ssh-agent for git server authentication.
+#
+# Support usage of ssh-agent for git server authentication. If you don't need that part just remove it.
+#
 # To install this file make symbolic link in /usr/bin directory:
 #
-#   ln -s <path-to-script>/git-update.sh /usr/bin
+#   sudo ln -s <path-to-script>/git-update.sh /usr/bin/git-update
 #
 
 
-#assuming ssh-agent is running
-#if no identity added yet add default one (assuming that it is used by git)
+# assuming ssh-agent is running
+# if no identity added yet add default one (assuming that it is used by git)
 ssh-add -l &> /dev/null
 if [ $? -eq 1 ]; then
     ssh-add
